ce0d166185cbf6ed35155d7fe5b9aa80449d3f0d
fracariprog <wesleyfracari1@gmail.com>
2020-06-07 20:35:19 -0300

    finalizando backend do app

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+    async index(request: Request, response: Response) {
+        const items = await knex('items').select('*');
+        const serealizedItems = items.map(item => {
+            return { 
+                id: item.id,
+                title: item.title,
+                image_url: `http://localhost:3333/uploads/${item.image}`
+            };
+
+        });
+    
+        return response.json(serealizedItems);
+    };
+
+};
+
+export default ItemsController;
